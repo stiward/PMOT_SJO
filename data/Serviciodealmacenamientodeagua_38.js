3da4d0ce7e3e4ab4c131f2b6f61b6fd99f600510
stiward <you@example.comstiwardm@gmail.com>
2024-10-24 19:48:07 -0400

    Primer commit del proyecto Leaflet

---
+++ data/Serviciodealmacenamientodeagua_38.js
@@ -0,0 +1,1 @@
+var json_Serviciodealmacenamientodeagua_38 = {"type":"FeatureCollection","name":"Serviciodealmacenamientodeagua_38","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null},"geometry":{"type":"Point","coordinates":[-70.377543126728582,18.54581464039104]}}]}
